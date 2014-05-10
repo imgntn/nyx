e25efb26b36068891c5940a1c496bb3b7e735564
James B. Pollack <jamesbradenpollack@gmail.com>
2014-05-10 00:00:42 -0700

    add files

---
+++ gulp/tasks/copy.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+
+gulp.task('copy', function(){
+  gulp.src('./index.html')
+    .pipe(gulp.dest('static-fileserver'));
+
+ gulp.src('./build/bundle.css')
+    .pipe(gulp.dest('static-fileserver/build/'));
+
+ 
+ gulp.src('./build/bundle.js')
+    .pipe(gulp.dest('static-fileserver/build/'));
+});
