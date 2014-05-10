48eefeeaf5228a142780cc375190f7bc6c5fb47e
James B. Pollack <jamesbradenpollack@gmail.com>
2014-05-10 01:56:34 -0700

    add router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
+Backbone.$=$;
 
 var myRootRef = new Firebase('https://jbpframework.firebaseIO.com/');
 myRootRef.set("hello");
 
-var AppView = require('./views/test');
+
+
+var Router = Backbone.Router.extend({
+  routes : {
+    ""    : "index",
+    "foo" : "foo",
+    "bar" : "bar"
+  },
+  index : function() {
+  	console.log('index')
+ var AppView = require('./views/test');
 var appView = new AppView();
+  },
+  foo : function() {
+    console.log("foo");
+  },
+  bar : function() {
+    console.log("bar");
+  }
+});
+ 
+new Router();
+ 
+Backbone.history.start();
